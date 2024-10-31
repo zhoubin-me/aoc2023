c7b94229a1058da94bd2733809d57b96dd6a5f97
ZHOU Bin <bin.zhou@dyson.com>
2024-10-31 11:49:38 +0800

    update

--- examples/d9.rs
+++ examples/d9.rs
@@ -1,72 +1,54 @@
-use std::collections::HashMap;
+
 use std::env;
 use std::fs;
-use num::{Integer, Num};
 
+use regex::Regex;
 
-pub fn gcd<T: Integer + Copy>(mut a: T, mut b: T) -> T {
-    while b != T::zero() {
-        let t = b;
-        b = a % b;
-        a = t;
-    }
-    a
-}
+fn part_one(content: &String) -> i64 {
+    let re = Regex::new(r"-?\d+").unwrap();
+    content.lines().map(
+        |line| {
+            let mut nums: Vec<i64> = re.find_iter(line).map(
+                |m| 
+                m.as_str().parse::<i64>().unwrap()
+            ).collect();
 
-pub fn lcm<T: Integer + Copy>(a: T, b: T) -> T {
-    a / gcd(a, b) * b
-}
+            let mut result = 0;
+            while !nums.iter().all(|&x| x == 0) {
+                result += nums.last().unwrap();
+                nums = nums.iter_mut().scan(0, |state, x| {
+                    let y = *x - *state;
+                    *state = *x;
+                    Some(y)
+                }).skip(1).collect();
+            }
 
-fn get_number_of_steps(
-    start: &str,
-    end: &str,
-    directions: &[u8],
-    nodes: &HashMap<&str, [&str; 2]>,
-) -> usize {
-    let mut current_node: &str = start;
-    for n_steps in 0.. {
-        if current_node == end || (end.is_empty() && current_node.ends_with('Z')) {
-            return n_steps;
+            result
         }
-        let direction: usize = if directions[n_steps % directions.len()] == b'L' {
-            0
-        } else {
-            1
-        };
-        current_node = nodes[current_node][direction];
-    }
-    unreachable!()
+    ).sum()
 }
 
-fn get_number_of_parallel_steps(directions: &[u8], nodes: &HashMap<&str, [&str; 2]>) -> u64 {
-    // This implementation assumes that every parallel path hits a Z node at a constant interval
-    // (this "Z interval" may be different for every parallel path).
-    let start_nodes: Vec<&str> = nodes
-        .keys()
-        .filter(|node| node.ends_with('A'))
-        .copied()
-        .collect();
-    let z_intervals = start_nodes
-        .iter()
-        .map(|node| get_number_of_steps(node, "", directions, nodes) as u64);
-    dbg!(z_intervals.collect::<Vec<u64>>());
-    // z_intervals.reduce(lcm).unwrap()
-    0
-}
+fn part_two(content: &String) -> i64 {
+    let re = Regex::new(r"-?\d+").unwrap();
+    content.lines().map(
+        |line| {
+            let mut nums: Vec<i64> = re.find_iter(line).map(
+                |m| 
+                m.as_str().parse::<i64>().unwrap()
+            ).collect();
 
-pub fn run(input: &str) {
-    let mut parts = input.split("\n\n");
-    let directions = parts.next().unwrap().as_bytes();
-    let nodes: HashMap<_, _> = parts
-        .next()
-        .unwrap()
-        .lines()
-        .map(|line| (&line[0..3], [&line[7..10], &line[12..15]]))
-        .collect();
-    let n_steps = get_number_of_steps("AAA", "ZZZ", directions, &nodes);
-    println!("{}", n_steps);
-    let n_steps = get_number_of_parallel_steps(directions, &nodes);
-    println!("{}", n_steps)
+            let mut result = vec![];
+            while !nums.iter().all(|&x| x == 0) {
+                result.push(nums.first().cloned().unwrap());
+                nums = nums.iter_mut().scan(0, |state, x| {
+                    let y = *x - *state;
+                    *state = *x;
+                    Some(y)
+                }).skip(1).collect();
+            }
+            result.iter().rev().fold(0, |state, x| x - state)
+        }
+    ).sum()
 }
 
 
@@ -79,6 +61,6 @@ fn main() {
     }
     let filename = &args[1];
     let content: String = fs::read_to_string(filename).expect("Cannot open file!");
-    // dbg!(part_one(&content));
-    dbg!(run(&content));
+    dbg!(part_one(&content));
+    dbg!(part_two(&content));
 }
